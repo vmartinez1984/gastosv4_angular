29934025c0399f7cf5cd9fc29176124c78983830
Víctor Martínez <ahal_tocob@hotmail.com>
2025-08-19 17:25:55 -0600

    Fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: "https://localhost:7275/api/"
-    //apiUrl: "http://localhost:1942/api/"
+    //apiUrl: "http://localhost:86/api/"
     //apiUrl: "https://gastos.vmartinez84.xyz/api/"
 };
